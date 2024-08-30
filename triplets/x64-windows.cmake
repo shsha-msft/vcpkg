5296532b95eeb0fe87338f806bb0ec17b1c5911e
Shishir Sharma <shsha@microsoft.com>
2024-08-30 15:03:15 -0700

    Specify /guard:cf, /Qspectre and /DYNAMICBASE Note: must set __VCVARSALL_SPECTRE=spectre before running vcpkg install

--- triplets/x64-windows.cmake
+++ triplets/x64-windows.cmake
@@ -2,3 +2,9 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
+set(VCPKG_C_FLAGS "/guard:cf /Qspectre")
+set(VCPKG_CXX_FLAGS "/guard:cf /Qspectre")
+set(VCPKG_LINKER_FLAGS "/guard:cf /DYNAMICBASE")
+ 
+# Set __VCVARSALL_SPECTRE=spectre before running vcpkg.exe
+set(VCPKG_ENV_PASSTHROUGH "__VCVARSALL_SPECTRE")
